b87306816248ba73e8251ceb2191839b028261eb
Izy <126877709+izy-code@users.noreply.github.com>
2024-03-19 22:02:27 +0300

    feat: add 4 solutions

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -100,8 +100,11 @@ function getCountDaysInMonth(month, year) {
  * '2024-02-01T00:00:00.000Z', '2024-02-02T00:00:00.000Z'  => 2
  * '2024-02-01T00:00:00.000Z', '2024-02-12T00:00:00.000Z'  => 12
  */
-function getCountDaysOnPeriod(/* dateStart, dateEnd */) {
-  throw new Error('Not implemented');
+function getCountDaysOnPeriod(dateStart, dateEnd) {
+  const startDate = new Date(dateStart);
+  const endDate = new Date(dateEnd);
+
+  return (endDate - startDate) / (1000 * 60 * 60 * 24) + 1;
 }
 
 /**
@@ -121,8 +124,12 @@ function getCountDaysOnPeriod(/* dateStart, dateEnd */) {
  * '2024-02-02', { start: '2024-02-02', end: '2024-03-02' } => true
  * '2024-02-10', { start: '2024-02-02', end: '2024-03-02' } => true
  */
-function isDateInPeriod(/* date, period */) {
-  throw new Error('Not implemented');
+function isDateInPeriod(date, period) {
+  const checkedDate = new Date(date);
+  const startDate = new Date(period.start);
+  const endDate = new Date(period.end);
+
+  return checkedDate >= startDate && checkedDate <= endDate;
 }
 
 /**
@@ -136,8 +143,8 @@ function isDateInPeriod(/* date, period */) {
  * '1999-01-05T02:20:00.000Z' => '1/5/1999, 2:20:00 AM'
  * '2010-12-15T22:59:00.000Z' => '12/15/2010, 10:59:00 PM'
  */
-function formatDate(/* date */) {
-  throw new Error('Not implemented');
+function formatDate(date) {
+  return new Date(date).toLocaleString('en-US', { timeZone: 'UTC' });
 }
 
 /**
@@ -152,8 +159,20 @@ function formatDate(/* date */) {
  * 12, 2023 => 10
  * 1, 2024 => 8
  */
-function getCountWeekendsInMonth(/* month, year */) {
-  throw new Error('Not implemented');
+function getCountWeekendsInMonth(month, year) {
+  const daysCountInMonth = new Date(year, month, 0).getDate();
+  let result = 0;
+
+  for (let dayOfMonth = 1; dayOfMonth <= daysCountInMonth; dayOfMonth += 1) {
+    const date = new Date(year, month - 1, dayOfMonth);
+    const dayOfWeekNumber = date.getDay();
+
+    if (dayOfWeekNumber === 0 || dayOfWeekNumber === 6) {
+      result += 1;
+    }
+  }
+
+  return result;
 }
 
 /**
